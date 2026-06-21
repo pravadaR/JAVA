36dc92e5ee8d2899317718c5046d9b561836cc71
pravadaR <pravada290407@gmail.com>
2026-06-21 17:40:58 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,36 @@
+import java.util.*; 
+
+public class FirstClass {
+    public static void main(String[] args)
+    {
+        try (Scanner sc = new Scanner(System.in)) {
+       
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println("Sum of a and b:" + sum);
+        
+             //String name = sc.next();
+            //System.out.println(name;
+        
+            System.out.println("Hello World! My name is Pravada!!");
+            System.out.print("How is life?\n");
+            System.out.print(":)\n");
+
+            System.out.println("*");
+            System.out.println("**");
+            System.out.println("***");
+            System.out.println("****");
+
+       
+            //int age = 19;
+            //double price = 99.99;
+            //int a = 25;
+            //int b = 10;
+            //int sum = a + b;
+            System.out.println("Sum of a and b:"+sum);
+        
+        }
+    }
+}
