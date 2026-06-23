1f60c650d2d0f70ce8e90da99d0e9854fdbd417a
pravadaR <pravada290407@gmail.com>
2026-06-23 20:01:44 +0530

    Add files via upload

---
+++ assi1.java
@@ -0,0 +1,80 @@
+public class assi1 {
+    public static void main(String arg[]) {
+        //System.out.println("Hello World");
+
+        //*variables */
+        // String name = "Pravada";
+        // int age = 19;
+
+        // String neighbour = "sonu";
+        // String friends = neighbour;
+        // System.out.println("Name: " + name);
+        // System.out.println("Age: " + age);
+        // System.out.println("Neighbour: " + neighbour);
+        // System.out.println("Friends: " + friends);
+
+        // String name = new String("Pravada");
+        // int[] marks = new int[3];
+        // marks[0] = 100;
+        // marks[1] = 90;
+        // marks[2] = 80;
+        // System.out.println("Name: " + name);
+        // System.out.println("Marks: " + marks[0] + ", " + marks[1] + ", " + marks[2]);
+
+        //Constants
+
+        // final float PI = 3.14f;
+        //system.out.println("Value of PI: " + PI);
+
+        //if-else
+        // int age = 30;
+        // if(age>18)
+        // {
+        //     System.out.println("You are eligible to vote.");
+        // }
+        // else
+        // {
+        //     System.out.println("You are not eligible to vote.");
+        // }
+        //         int n = 4;
+        // switch(n) {
+        //    case 1 :
+        //        System.out.println("Monday");
+        //        break;
+        //    case 2 :
+        //        System.out.println("Tuesday");
+        //        break;
+        //    case 3 :
+        //        System.out.println("Wednesday");
+        //        break;
+        //    case 4 :
+        //        System.out.println("Thursday");
+        //        break;
+        //    case 5:
+        //        System.out.println("Friday");
+        //        break;
+        //    case 6 :
+        //        System.out.println("Saturday");
+        //        break;
+        //    default :
+        //        System.out.println("Sunday");
+        // }
+
+        // for(int i=1;i<=20;i++)
+        // {
+        //     System.out.println(i);
+        // }
+        // int i = 0;
+        // while(i<=20)
+        // {
+        //     System.out.println(i);
+        //     i++;
+        // }
+        //  i = 0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<=20);
+    }
+
+}
