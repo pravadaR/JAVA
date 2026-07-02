f02b01324a57b8d99466a8413f68a5b5f79c7563
pravadaR <pravada290407@gmail.com>
2026-07-02 19:45:12 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,76 @@
+import java.util.*;
+public class Strings {
+    public static void main(String args[])
+    {
+        //string declaration
+        // String name = "Pravada";
+        // String fullName = "Pravada Raval";
+        // String sentence="My name is Pravada Raval";
+
+        //user input
+        // try(Scanner sc=new Scanner(System.in))
+        // {
+        //     String name = sc.nextLine();
+        //     System.out.println("Your name is:"+name);
+
+        // }
+
+        //Concatenation
+        //  String firstname = "Pravada";
+        //  String lastname = "Raval";
+        //  String fullname = firstname + " " + lastname;
+        //  System.out.println(fullname.length());
+
+        // //charAt
+        // for(int i=0;i<fullname.length();i++)
+        // {
+        //     System.out.println(fullname.charAt(i));
+        // }
+
+        //compare
+        String name1 = "Pravada";
+        String name2 = "Pravada";
+
+        //1 s1>s2 :+ve value
+        //2 s1==s2 :0
+        //3 s1<s2 :-ve value
+
+        // if (name1.compareTo(name2) == 0)
+        // {
+        //     System.out.println("Both names are same");
+        // }
+        // else
+        // {
+        //     System.out.println("Both names are different");
+        // }
+
+        // if (new String("Pravada").equals(new String("Pravada"))) {
+        //     System.out.println("Both names are same");
+        // } else {
+        //     System.out.println("Both names are different");
+        // }
+
+        // String sentence = "Pravada Raval";
+        // String name = sentence.substring(0, 7);
+        // System.out.println(name);
+
+        //*Strings are Immutable*
+
+        //Declaration
+        // String name = "Pravada";
+
+        //Taking i/p
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+
+        //ParseInt Method of Integer class
+        // String str = "123";
+        // int num = Integer.parseInt(str);
+        // System.out.println(num);
+
+        //ToString Method of String class
+        // int number = 123;
+        // String str = Integer.toString(number);
+        // System.out.println(str.length());
+    }
+}
