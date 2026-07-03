204f1846d096243dda5c7be9bb5cd71aee716e8a
pravadaR <pravada290407@gmail.com>
2026-07-03 12:17:22 +0530

    Add files via upload

--- Strings.java
+++ Strings.java
@@ -1,4 +1,4 @@
-import java.util.*;
+//import java.util.*;
 public class Strings {
     public static void main(String args[])
     {
@@ -28,8 +28,8 @@ public class Strings {
         // }
 
         //compare
-        String name1 = "Pravada";
-        String name2 = "Pravada";
+        // String name1 = "Pravada";
+        // String name2 = "Pravada";
 
         //1 s1>s2 :+ve value
         //2 s1==s2 :0
@@ -72,5 +72,43 @@ public class Strings {
         // int number = 123;
         // String str = Integer.toString(number);
         // System.out.println(str.length());
+
+
+        ////StringBuilder class
+        StringBuilder sb = new StringBuilder("Pravada");
+        System.out.println(sb);
+
+        //char at index 0
+        System.out.println(sb.charAt(0));
+
+        //set char at index 0
+        sb.setCharAt(0, 'p');
+        System.out.println(sb);
+
+        sb.insert(0, "Hello ");
+        System.out.println(sb);
+        
+        sb.delete(0, 6);
+        System.out.println(sb);
+        sb.append(" ");
+        sb.append("R");
+        sb.append("a");
+        sb.append("v");
+        sb.append("a");
+        sb.append("l");
+        System.out.println(sb);
+        System.out.println(sb.length());
+
+        sb = new StringBuilder("Hello");
+        for(int i=0;i<sb.length()/2;i++)
+        {
+            int front = i;
+            int back = sb.length() - 1 - i;//5-1-0=4
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+        System.out.println(sb);
     }
 }
