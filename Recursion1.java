5c641120fa81c54fecc11b9c0d46d8f24416f8a6
pravadaR <pravada290407@gmail.com>
2026-07-07 14:00:03 +0530

    Add files via upload

---
+++ Recursion1.java
@@ -0,0 +1,88 @@
+//import java.util.*;
+public class Recursion1 {
+    //5 to 1
+    // public static void printNumb(int n)
+    // {
+    //     if (n == 0) {
+    //         return;
+    //     }
+    //     System.out.println(n);
+    //     printNumb(n - 1);
+    // }
+
+    // public static void main(String args[])
+    // {
+    //     int n = 5;
+    //     printNumb(n);
+    // }
+
+    //1 to 5
+    // public static void printNumb(int n)
+    // {
+    //     if (n == 6) {
+    //         return;
+    //     }
+    //     System.out.println(n);
+    //     printNumb(n + 1);
+    // }
+
+    // public static void main(String args[])
+    // {
+    //     int n = 1;
+    //     printNumb(n);
+    // }
+
+    //sum of first n natural numbers
+    // public static void printSum(int i, int sum, int n)
+    // {
+    //     if (i == n) {
+    //         sum += i;
+    //         System.out.println(sum);
+    //         return;
+    //     }
+    //     sum += i;
+    //     printSum(i + 1, sum, n);
+    // }
+    // public static void main(String args[])
+    // {
+    //     printSum(1, 0, 5);
+    // }
+
+    //factorial of a number n
+    // public static int printFac(int n)
+    // {
+    //     if(n==1 || n==0)
+    //     {
+    //         return 1;
+    //     }
+    //     int fact_nm1=printFac(n-1);
+    //     int fact_n=n*fact_nm1;
+    //     return fact_n;
+    // }
+    // public static void main(String args[])
+    // {
+    //     int n = 3;
+    //     int ans = printFac(n);
+    //     System.out.println(ans);
+    // }
+
+    //fibonacci series
+    //public static void printFib(int a, int b, int n)
+    // {
+    //     if(n==0)
+    //     {
+    //         return;
+    //     }
+    //     int c = a + b;
+    //     System.out.println(c);
+    //     printFib(b, c, n-1);  
+    // }
+    // public static void main(String args[])
+    // {
+    //     int a = 0, b = 1;
+    //     System.out.println(a);
+    //     System.out.println(b);
+    //     int n = 7;
+    //     printFib(a, b, n - 2);
+    // }
+}
